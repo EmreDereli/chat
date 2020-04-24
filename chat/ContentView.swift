566f0845c2f642df5066bc6b4bb939c47eed8854
EmreDereli <emredereli35@hotmail.com>
2020-04-24 04:59:39 -0700

    first commit

---
+++ chat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chat
+//
+//  Created by Canberk BİBİCAN on 24.04.2020.
+//  Copyright © 2020 Emre Dereli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
